3330c46493881761cb3cd76f9703ec2462a793e9
arvidlarzzon <arvidlarzzon@hotmail.com>
2018-11-29 20:28:08 +0100

    add new js structure, scrolling is still fckd

--- js/app.js
+++ js/app.js
@@ -1,186 +1,146 @@
-// When the user scrolls the page, execute scrollHandler
-// window.onscroll = () => {
-//   scrollHandler();
-// };
-var timeoutBool = true;
-var timeout = 3000; //ms
-var currentChapter = 1;
-var lastScroll = 0;
+function App() {
+  // App is initiated in a script tag in index.html
 
-window.onscroll = function(e) {
-  // print "false" if direction is down and "true" if up
-  console.log(this.oldScroll > this.scrollY);
-  this.oldScroll = this.scrollY;
-};
+  // Elements
+  var stickybar = document.getElementById('stickybar');
+  var contentContainer = document.getElementById('content-container');
+  var chapters = document.getElementsByClassName('chapter-row');
 
-var contentContainer = document.getElementsByClassName('content-container')[0];
-var contentHeight = contentContainer.getBoundingClientRect().height;
-var chapters = document.getElementsByClassName('chapter-row');
-for (var i = 0; i < chapters.length; i++) {
-  chapters[i].style.height = contentHeight + 'px';
-}
+  // Variables and constants
+  const app = this;
+  const timeout = 1000; //ms
+  const contentHeight = contentContainer.getBoundingClientRect().height;
+  const contentObject = {
+    // Kan bytas till json-fil
+    chapter1: {
+      name: 'Authentication',
+      paper: `In the Swedish voting system, all eligible voters receives a
+      voting card by mail before the election day. Sweden has
+      <b>universal suffrage</b> to Swedish residents over the age of 18
+      years.`,
+      electronic: `Electronic voting systems also have electoral rolls and the
+      <b>universial suffrage</b> would be no different in an electronic
+      voting system.`,
+    },
+    chapter2: {
+      name: 'Privacy',
+      paper: `Lorem ipsum`,
+      electronic: `Lorem ipsum`,
+    },
+    chapter3: {
+      name: 'Trust',
+      paper: `Lorem ipsum`,
+      electronic: `Lorem ipsum`,
+    },
+    chapter4: {
+      name: 'Privacy',
+      paper: `Lorem ipsum`,
+      electronic: `Lorem ipsum`,
+    },
+  };
+  let timeoutBool = true;
+  let currentChapter = 0; // 0=intro,1=chapter1
+  let lastScroll = contentContainer.scrollTop;
 
-contentContainer.addEventListener('scroll', e => {
-  scrollHandler();
-  var st = contentContainer.scrollTop;
+  // Initiating functions
+  for (var i = 0; i < chapters.length; i++) {
+    chapters[i].style.height = Math.round(contentHeight, 0) + 'px';
+    chapters[i].style.margin = '10px';
+  }
 
-  if (timeoutBool) {
-    timeoutBool = false;
-    if (st > lastScroll) {
-      // Scrolling down
-      console.log('down');
+  this.nextChapter = () => {
+    if (currentChapter < 4) {
+      currentChapter++;
+      updateChapter();
+    }
+  };
+  this.prevChapter = () => {
+    if (currentChapter > 0) {
+      currentChapter--;
+      updateChapter();
+    }
+  };
+  this.goToChapter = n => {
+    currentChapter = n;
+    updateChapter();
+  };
+  this.getChapter = () => {
+    return currentChapter;
+  };
+
+  this.getChapterElement = () => {
+    var chapterElement = chapters[currentChapter - 1];
+    return chapterElement;
+  };
+
+  function updateChapter() {
+    console.log(currentChapter);
+    var chapter = app.getChapterElement();
+    var scrollIntoViewOptions = {
+      behavior: 'smooth',
+    };
+    if (chapter) {
+      chapter.scrollIntoView(scrollIntoViewOptions);
     } else {
-      // Scrolling up
-      console.log('up');
+      console.log('Chapter: ', app.getChapter(), ' does not exist.');
+    }
+  }
+
+  contentContainer.addEventListener('scroll', e => {
+    scrollHandler();
+    var st = contentContainer.scrollTop;
+    if (timeoutBool) {
+      disableScroll();
+      timeoutBool = false;
+      if (st > lastScroll) {
+        // Scrolling down
+        app.nextChapter();
+      } else {
+        // Scrolling up
+        app.prevChapter();
+      }
+      window.setTimeout(() => {
+        timeoutBool = true;
+        enableScroll();
+      }, timeout);
     }
     lastScroll = st <= 0 ? 0 : st;
-    window.setTimeout(() => {
-      timeoutBool = true;
-      console.log(timeoutBool);
-    }, timeout);
+  });
+  function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    // Only completely visible elements return true:
+    var isVisible = elemTop < window.innerHeight;
+    // Partially visible elements return true:
+    // isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
   }
-});
-function isScrolledIntoView(el) {
-  var rect = el.getBoundingClientRect();
-  var elemTop = rect.top;
-  var elemBottom = rect.bottom;
 
-  // Only completely visible elements return true:
-  var isVisible = elemTop <= window.innerHeight;
-  // Partially visible elements return true:
-  //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-  return isVisible;
-}
-function scrollHandler() {
-  if (window.pageYOffset >= sticky) {
-    stickybar.classList.add('sticky');
-  } else {
-    stickybar.classList.remove('sticky');
+  function scrollHandler() {
+    var chap1 = document.getElementById('chapter1');
+    var chap2 = document.getElementById('chapter2');
+    var chap3 = document.getElementById('chapter3');
+    var chap4 = document.getElementById('chapter4');
+
+    if (isScrolledIntoView(chap1)) {
+      document.getElementById('chap1').style.backgroundColor = '#059ada';
+    } else {
+      document.getElementById('chap1').style.backgroundColor = '#0b7eb5';
+    }
+    if (isScrolledIntoView(chap2)) {
+      document.getElementById('chap2').style.backgroundColor = '#059ada';
+    } else {
+      document.getElementById('chap2').style.backgroundColor = '#0b7eb5';
+    }
+    if (isScrolledIntoView(chap3)) {
+      document.getElementById('chap3').style.backgroundColor = '#059ada';
+    } else {
+      document.getElementById('chap3').style.backgroundColor = '#0b7eb5';
+    }
+    if (isScrolledIntoView(chap4)) {
+      document.getElementById('chap4').style.backgroundColor = '#059ada';
+    } else {
+      document.getElementById('chap4').style.backgroundColor = '#0b7eb5';
+    }
   }
-  var header = document.getElementById('stickybar').getBoundingClientRect();
-  var chap1 = document.getElementById('chapter1');
-  var chap1rect = chap1.getBoundingClientRect();
-  var chap2 = document.getElementById('chapter2');
-  var chap2rect = chap2.getBoundingClientRect();
-  var chap3 = document.getElementById('chapter3');
-  var chap3rect = chap3.getBoundingClientRect();
-  var chap4 = document.getElementById('chapter4');
-  var chap4rect = chap4.getBoundingClientRect();
-
-  if (isScrolledIntoView(chap1)) {
-    document.getElementById('chap1').style.backgroundColor = '#059ada';
-  } else {
-    document.getElementById('chap1').style.backgroundColor = '#0b7eb5';
-  }
-  if (isScrolledIntoView(chap2)) {
-    document.getElementById('chap2').style.backgroundColor = '#059ada';
-  } else {
-    document.getElementById('chap2').style.backgroundColor = '#0b7eb5';
-  }
-  if (isScrolledIntoView(chap3)) {
-    document.getElementById('chap3').style.backgroundColor = '#059ada';
-  } else {
-    document.getElementById('chap3').style.backgroundColor = '#0b7eb5';
-  }
-  if (isScrolledIntoView(chap4)) {
-    document.getElementById('chap4').style.backgroundColor = '#059ada';
-  } else {
-    document.getElementById('chap4').style.backgroundColor = '#0b7eb5';
-  }
-}
-
-var stickybar = document.getElementById('stickybar');
-var sticky = stickybar.offsetTop;
-
-// Get the modal
-var modal = document.getElementById('myModal');
-var modal2 = document.getElementById('myModal2');
-
-// Get the button that opens the modal
-var btn = document.getElementById('myBtn');
-var btn2 = document.getElementById('myBtn2');
-
-// Get the <span> element that closes the modal
-var span = document.getElementById('close');
-var span2 = document.getElementById('close2');
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = 'block';
-};
-btn2.onclick = function() {
-  modal2.style.display = 'block';
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = 'none';
-};
-span2.onclick = function() {
-  modal2.style.display = 'none';
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-  if (event.target == modal2) {
-    modal2.style.display = 'none';
-  }
-};
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n /*, index*/) {
-  // Senare kan vi lägga till en inparameter med index för att vet vilka slides vi ska hämta
-  var i;
-  var slides = document.getElementsByClassName('mySlides' /* + index */);
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slides[slideIndex - 1].style.display = 'block';
-}
-
-var slideIndex2 = 1;
-showSlides2(slideIndex2);
-// Next/previous controls
-function plusSlides2(n) {
-  showSlides2((slideIndex2 += n));
-}
-
-// Thumbnail image controls
-function currentSlide2(n) {
-  showSlides2((slideIndex2 = n));
-}
-
-function showSlides2(n) {
-  var j;
-  var slides2 = document.getElementsByClassName('mySlides2');
-  if (n > slides2.length) {
-    slideIndex2 = 1;
-  }
-  if (n < 1) {
-    slideIndex2 = slides2.length;
-  }
-  for (j = 0; j < slides2.length; j++) {
-    slides2[j].style.display = 'none';
-  }
-  slides2[slideIndex2 - 1].style.display = 'block';
 }
